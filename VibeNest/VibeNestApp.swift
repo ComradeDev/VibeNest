ac749d6f57b7350b1b67ec4e3f58257f43f54978
ComradeDev <sam.r.moen@gmail.com>
2025-04-27 14:43:53 -0400

    Initial Commit

---
+++ VibeNest/VibeNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeNestApp.swift
+//  VibeNest
+//
+//  Created by sam moen on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct VibeNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibeNestTests/VibeNestTests.swift
@@ -0,0 +1,17 @@
+//
+//  VibeNestTests.swift
+//  VibeNestTests
+//
+//  Created by sam moen on 4/27/25.
+//
+
+import Testing
+@testable import VibeNest
+
+struct VibeNestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
